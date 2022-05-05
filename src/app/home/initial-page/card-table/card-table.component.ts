53752dbea6e76f4bdf0b0cd798d5da775e1dc35f
JoseJSL <josehyvy2@gmail.com>
2022-05-04 19:55:01 -0600

    Arreglado problema con elementos expandibles de la tabla

--- src/app/home/initial-page/card-table/card-table.component.ts
+++ src/app/home/initial-page/card-table/card-table.component.ts
@@ -1,23 +1,34 @@
 import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTable, MatTableDataSource } from '@angular/material/table';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { Egreso, Ingreso, Traslado } from 'src/app/service/cfdi';
 
 @Component({
   selector: 'app-card-table',
   templateUrl: './card-table.component.html',
-  styleUrls: ['./card-table.component.scss']
+  styleUrls: ['./card-table.component.scss'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class CardTableComponent implements AfterViewInit {
-  @ViewChild('dataTable') dataTable!: MatTable<any>;
-  @Input() inputData!: any[];
   @ViewChild('tablePaginator') tablePaginator!: MatPaginator;
-  public tableDataSource!: MatTableDataSource<any>;
+  @ViewChild('dataTable') dataTable!: MatTable<(Ingreso | Egreso | Traslado)>;
 
+  @Input() inputData!: (Ingreso | Egreso | Traslado)[];
   @Input() tableColumns: string[] = [];
   @Input() noContentLabel: string | undefined;
   @Input() noContentSubLabel: string | undefined;
   @Input() onNoContentReRoute: string | any[] | null | undefined;
 
+  public tableDataSource!: MatTableDataSource<(Ingreso | Egreso | Traslado)>;
+  public expandedElement?: (Ingreso | Egreso | Traslado);
+  
   constructor() {}
 
   ngAfterViewInit(): void {
@@ -31,7 +42,7 @@ export class CardTableComponent implements AfterViewInit {
   }
 
   formatAsMoney(quantity: number) : string{
-    return '$' + quantity.toFixed(2);;
+    return '$' + quantity.toFixed(2);
   }
 
   getConcepts(Conceptos: any[]): string{
